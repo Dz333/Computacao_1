c16360b8e106fb2436347a516e8dc69bc768e46c
Dz333 <nzamba@alunos.utfpr.edu.br>
2025-06-18 10:40:58 -0300

    Update arquivos_2.c

--- arquivos_2.c
+++ arquivos_2.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
+#define TAM 100
 #define VALORES "teste2.txt"
 
 int main ()
 {
     FILE* t2;
     float n, total;
+    char string [TAM];
 
     t2 = fopen (VALORES, "r");
     if (t2 == NULL)
         return (1);
 
     while (fscanf (t2, "%f", &n) != EOF)
-    {
-        printf ("Leu %.2f\n", n);
         total += n;
-    }
 
     printf ("Total: %.2f\n", total);
+
+    fprintf (t2, "%d\n", total);
+
+    while (fgets (string, TAM, t2))
+    printf ("%s", string);
+
 
     fclose (t2);
 
